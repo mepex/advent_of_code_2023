fbb425660f6191b2fa981b84661667755370f61e
mepex <mike.peattie@gmail.com>
2024-12-11 13:36:57 -0800

    done 2024/11 part 2, using hashes

--- 2024/day11/main.py
+++ 2024/day11/main.py
@@ -1,8 +1,8 @@
 from mymodule import *
-import re
+from collections import Counter
 
-lines = get_lines("sample.txt")
-d = [int(x) for x in lines[0].split()]
+lines = get_lines("input.txt")
+golden = [int(x) for x in lines[0].split()]
 
 
 def blink(d: list):
@@ -22,11 +22,38 @@ def blink(d: list):
             d[i] = n * 2024
         i += 1
 
-for i in range(25):
-    blink(d)
-print(f"part 1: {len(d)}")
+def blink2(d: Counter):
+    c = Counter()
+    ks = list(d)
+    for k in ks:
+        v = d[k]
+        if k == 0:
+            c[1] = v if 1 not in c else v + c[1]
+        elif not len(str(k)) % 2:
+            l = len(str(k))
+            n1 = int(str(k)[:l // 2])
+            n2 = int(str(k)[l // 2:])
+            c[n1] = v if n1 not in c else v + c[n1]
+            c[n2] = v if n2 not in c else v + c[n2]
+        else:
+            c[k * 2024] = v if k*2024 not in c else v + c[n*2024]
+    return c
 
-memo = []
+
+
+d = Counter(golden)
+d1 = golden.copy()
+for i in range(25):
+    d = blink2(d)
+
+print(f"part 1: {sum(d.values())}")
+
+for i in range(55):
+    d = blink2(d)
+
+print(f"part 2: {sum(d.values())}")
+
+
 
 
 
